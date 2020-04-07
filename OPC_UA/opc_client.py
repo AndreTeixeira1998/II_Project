d8c689420aa087d462aa2dee99867004af191f76
Pedro Castro <up201605469@fe.up.pt>
2020-04-07 16:18:41 +0100

    Test array

--- OPC_UA/opc_client.py
+++ OPC_UA/opc_client.py
@@ -16,30 +16,37 @@ logging.basicConfig(level=logging.DEBUG, filename=LOG_FILENAME)
 _logger = logging.getLogger('asyncua')
 
 
-class piece():
-    '''
+class Piece():
+	'''
+	Há de fazer grandes coisas esta classe
 
-    '''
-    def __init__(self, id, var, optimizer):
-        self.id = id
-		self.var=var
+	'''
+	def __init__(self, id, var, optimizer):
+		self.id = id
+		self.var = var
 		self.optimizer=optimizer
-        self.waiting_time = 0
+		self.waiting_time = 0
 
-    def __str__(self):
-        return self.id
+	def __str__(self):
+		return self.id
 		
 	async def write_int16(self, var, value):
-		datavalue = ua.DataValue(ua.Variant(self.value, ua.VariantType.Int16))
+		datavalue = ua.DataValue(ua.Variant(value, ua.VariantType.Int16))
 		await var.write_value(datavalue)
 
 	async def write_bool(self, var, value):
-		datavalue = ua.DataValue(ua.Variant(self.value, ua.VariantType.Boolean))
+		datavalue = ua.DataValue(ua.Variant(value, ua.VariantType.Boolean))
 		await var.write_value(datavalue)
+
+	def compute_conveyor(self, before, after):
+		duration, piece, trans_path = self.optimizer.compute_transform(before, after)
+		path_to_write = self.optimizer.compute_path(trans_path)
+		return path_to_write
 
 
 
 async def write_int16(var, value):
+	#	datavalue = ua.DataValue(ua._val_to_variant(value, Int16))
 	datavalue = ua.DataValue(ua.Variant(value, ua.VariantType.Int16))
 	await var.write_value(datavalue)
 
@@ -55,7 +62,7 @@ async def write_array_int16(array, value):
 async def write(client, var, optimizer):
 	print("######################debug: write() started")
 	
-	
+	p = Piece(1, var, optimizer)
 	duration, piece, trans_path = optimizer.compute_transform("P2", "P5")
 	
 	path_to_write = optimizer.compute_path(trans_path)
@@ -68,7 +75,7 @@ async def write(client, var, optimizer):
 			
 			#try:
 			print("###############################    Changing Value!   ###########################")
-			await write_array_int16(var, path_to_write) # set node value using implicit data type
+			await write_int16(var, path_to_write) # set node value using implicit data type
 			#except:
 			#	print("!!!!!!!!!!!!!!!!!!!!!!!  ERROR  Changing Value!   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
@@ -95,7 +102,7 @@ async def main():
 		#root = client.get_root_node()
 		#program = await root.get_child(['0:Objects', '0:Server', '4:CODESYS Control Win V3 x64', '3:Resources', '4:Application', '3:Programs', '4:PLC_PRG'])
 		#vars = await program.get_children()
-		var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.piece_array[0]") ###variavel teste
+		var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.piece_array[0].path") ###variavel teste
 
 		await asyncio.gather(read(client, var, optimizer), write(client, var, optimizer))
 		
